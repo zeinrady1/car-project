0c509f90d4411227c91dfa22b5047b807ff566ef
Zein Rady <czvfyc55yp@privaterelay.appleid.com>
2026-07-25 12:02:52 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,23 @@
-radio.onReceivedNumber(function (receivedNumber) {
-	
+radio.onReceivedValue(function (name, value) {
+    let y = 0
+    if (name == "x") {
+        let speed = 0
+        jx = value
+        if (jx > 500 && jx < 525) {
+            jx = 512
+        }
+        x = Math.map(jx, 0, 1023, 0 - speed, speed)
+    }
+    if (0 == y) {
+        jy = value
+        if (name == "x") {
+        	
+        }
+    }
+    serial.writeValue(name, value)
 })
-input.onButtonPressed(Button.A, function () {
-	
-})
-basic.forever(function () {
-	
-})
+let jy = 0
+let x = 0
+let jx = 0
+radio.setGroup(1)
+radio.setFrequencyBand(1)
